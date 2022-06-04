09b69edb6b08131cc108d18e5dec7dc80d312785
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:32:55 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/dp_g/20503472.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long
+#define INF INT_MAX
+
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    string s; cin >> s; int n = s.size();
+    string t; cin >> t; int m = t.size();
+
+    int dp[n+1][m+1];
+    for (int i = 0; i <= n; i++)
+        for (int j = 0; j <= m; j++)
+            dp[i][j] = 0;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            if (s[i-1] == t[j-1])
+                dp[i][j] = dp[i-1][j-1] + 1;
+        }
+    }
+
+    string ans;
+    for (int i = n, j = m; i > 0 && j > 0;) {
+        if (s[i-1] == t[j-1]) ans += s[i-1], i--, j--;
+        else if (dp[i][j] == dp[i-1][j]) i--;
+        else if (dp[i][j] == dp[i][j-1]) j--;
+    }
+
+    reverse(ans.begin(), ans.end());
+    cout << ans << endl;
+}
