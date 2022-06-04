2ec0f498f43846f56d79482cc0599914a06c88be
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:35:59 +0530

    Committing files

---
+++ CodeChef/C++14/STADIUM/28698632.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool compare(const pair<int, int> &s, const pair<int, int> &d){
+    if(s.first < d.first) return true;
+    if(s.first > d.first) return false;
+
+    return s.second < d.second;
+}
+
+int main()
+{
+    int n; cin >> n;
+
+    pair<int, int> a[n];
+
+    for(int i = 0; i < n; i++){
+        cin >> a[i].first >> a[i].second;
+        a[i].second += a[i].first;
+    }
+
+    sort(a, a + n, compare);
+
+    int j = 1;
+
+    for(int i = 1; i < n; i++){
+        if(a[i].first != a[i-1].first){
+            a[j++] = a[i];
+        }
+    }
+
+    n = j;
+
+    int dp[n];
+
+    for(int i = 0; i < n; i++){
+        dp[i] = 1;
+
+        for(int j = i - 1; j >= 0; j--){
+            if(a[j].second < a[i].first){
+                dp[i] = dp[j] + 1;
+                break;
+            }
+        }
+    }
+
+    cout << *max_element(dp, dp + n) << endl;
+}
