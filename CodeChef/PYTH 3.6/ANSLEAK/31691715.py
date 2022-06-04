0bb3a0d0f91db7d33eb0954d6736f6c8ac916256
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:44:26 +0530

    Committing files

---
+++ CodeChef/PYTH 3.6/ANSLEAK/31691715.py
@@ -0,0 +1,13 @@
+from random import choice
+
+for _ in range(int(input())):
+    n, m, k = map(int, input().split())
+
+    d = []
+
+    for i in range(n):
+        a = list(map(int, input().split()))
+
+        d.append(choice(a))
+
+    print(*d)
