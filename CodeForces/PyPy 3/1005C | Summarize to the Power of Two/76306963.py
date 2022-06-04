25ce009430da7c36fcaca26e81c9cb746bfca2f7
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 11:17:08 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1005C | Summarize to the Power of Two/76306963.py
@@ -0,0 +1,28 @@
+from collections import Counter
+ 
+pows = [1]
+ 
+for i in range(64 + 1):
+    pows.append(pows[-1] * 2)
+ 
+n = int(input())
+a = list(map(int, input().split()))
+ 
+x = Counter(a)
+ 
+ans = 0
+ 
+for i in a:
+    for p in pows:
+        j = p - i
+ 
+        if j in x:
+            if i == j:
+                if x[i] >= 2:
+                    break
+            else:
+                break
+    else:
+        ans += 1
+ 
+print(ans)
