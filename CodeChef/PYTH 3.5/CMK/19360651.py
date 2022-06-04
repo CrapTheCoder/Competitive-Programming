b8f8ad9740e48720d2a89a8203823a7c0b76f741
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:52:49 +0530

    Committing files

---
+++ CodeChef/PYTH 3.5/CMK/19360651.py
@@ -0,0 +1,19 @@
+for _ in range(int(input())):
+    a = 0
+    n, k = map(int, input().split())
+    
+    try:
+    	m = sorted(map(int, input().split()))[:k]
+    	c = sorted(map(int, input().split()), reverse=True)[:k][::-1]
+    	
+    	for i in range(k):
+    	    if m[i] < c[i]:
+    	        a += 1
+            
+    except:
+    	pass
+
+    if a >= k:
+        print('YES')
+    else:
+        print('NO')
