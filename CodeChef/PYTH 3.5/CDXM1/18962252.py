056ffa9464f78ec82e06605055a40c0c326efd2a
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 11:00:56 +0530

    Committing files

---
+++ CodeChef/PYTH 3.5/CDXM1/18962252.py
@@ -0,0 +1,9 @@
+s = input()
+t = int(input())
+while t > 0:
+    a, b = map(int, input().split())
+    if s[(a - 1) % len(s)] == s[(b - 1) % len(s)]:
+        print("Yes")
+    else:
+        print("No")
+    t -= 1
