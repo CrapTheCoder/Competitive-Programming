6f59697e41927330ed7d3573f8d366851cd12498
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 11:16:20 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1353C | Board Moves/80094994.py
@@ -0,0 +1,16 @@
+for _ in range(int(input())):
+    n = int(input())
+    c = n // 2, n // 2
+ 
+    s = 0
+ 
+    for j in range(n):
+        s += max(abs(c[0] - 0), abs(c[1] - j))
+ 
+    ts = s
+ 
+    for i in range(1, n // 2 + 1):
+        s -= n - 2 * i
+        ts += s
+ 
+    print(ts * 2 - s)
