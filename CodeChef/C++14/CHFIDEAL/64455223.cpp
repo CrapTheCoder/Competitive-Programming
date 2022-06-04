d15d4e71bbf9e4c60425d243d0d1db24d19e8eed
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:57:09 +0530

    Committing files

---
+++ CodeChef/C++14/CHFIDEAL/64455223.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+
+signed main() {
+    cout << 1 << endl;
+
+    int y; cin >> y;
+
+    if (y == 2)
+        cout << 3 << endl;
+    else
+        cout << 2 << endl;
+}
