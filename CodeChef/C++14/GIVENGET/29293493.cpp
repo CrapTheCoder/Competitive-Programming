a65528d7364054be3f98aca97ee7db56fc51c356
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:45:56 +0530

    Committing files

---
+++ CodeChef/C++14/GIVENGET/29293493.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef int integer;
+typedef long long int ll;
+
+#define int ll
+
+#define endl '\n'
+
+integer main(){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+
+    while(t--){
+        int n; cin >> n;
+
+        cout << n * 2 << endl;
+    }
+}
