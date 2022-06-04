e4a0dfa3a6785f5f134d797efe1035c7c7fd4731
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:50:33 +0530

    Committing files

---
+++ CodeChef/C++14/AMONGUS2/56497017.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+long long get_rand() {
+    long long a = rand();
+    long long b = rand();
+    return a * (RAND_MAX + 1ll) + b;
+}
+
+#define int long long
+//#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+//#define ordered_multiset tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update>
+
+const int intmin = -100000000000000000LL;
+const int intmax = 100000000000000000LL;
+
+vector<pair<int, int>> adj[500001]; //node, type of edge
+vector<int> visited(500001, 0), color(500001, 0);
+
+int one = 0, siz = 0;
+bool dfs(int node, int col){
+    if (visited[node])
+        return color[node] == col;
+    visited[node] = 1;
+
+    siz++;
+    color[node] = col;
+    if(col == 0){
+        one++;
+    }
+
+    for (pair<int, int> child : adj[node])
+    {
+        if(child.second == 0){
+            if (!dfs(child.first, !col))
+                return false;
+        }
+        else{
+            if (!dfs(child.first, col))
+                return false;
+        }
+    }
+
+    return true;
+}
+
+int32_t main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+    int T;
+    cin >> T;
+    while(T--){
+        one = 0;
+        siz = 0;
+
+        int n, q;
+        cin >> n >> q;
+
+        //edge types: 1 for same team, 0 for different team
+        for (int i = 0; i < q; ++i)
+        {
+            int type, a, b;
+            cin >> type >> a >> b;
+            type--;
+
+            adj[a].push_back({b, type});
+            adj[b].push_back({a, type});
+        }
+
+        bool done = false;
+        int ans = 0;
+        for (int i = 1; i <= n; ++i)
+        {
+            if(visited[i] == 0){
+                if(!dfs(i, 0)){
+                    done = true;
+                    break;
+                }
+                else{
+                    ans += max(one, siz-one);
+                    one = 0;
+                    siz = 0;
+                }
+            }
+        }
+
+        if(done){
+            cout << "-1\n";
+        }
+        else{
+            cout << ans << '\n';
+        }
+
+        for (int i = 0; i <= n; ++i)
+        {
+            adj[i].clear();
+            color[i] = 0;
+            visited[i] = 0;
+        }
+    }
+}
