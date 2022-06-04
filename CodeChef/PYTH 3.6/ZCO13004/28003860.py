8c73b6b8dc055d2f7df78b56f2b1833c989825da
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:51:33 +0530

    Committing files

---
+++ CodeChef/PYTH 3.6/ZCO13004/28003860.py
@@ -0,0 +1,44 @@
+n, m, k = map(int, input().split())
+
+ls = [[1 for _ in range(m)] for _ in range(n)]
+
+for _ in range(k):
+    r, c, s, f = map(int, input().split())
+
+    r -= 1
+    c -= 1
+
+    ls[r][c] = 0
+
+    for i in range(1, n):
+        x = (i + c) - (s + abs(i - r))
+
+        if x >= 0 and not x % f:
+            ls[i][c] = 0
+
+    for j in range(1, m):
+        x = (r + j) - (s + abs(j - c))
+
+        if x >= 0 and not x % f:
+            ls[r][j] = 0
+
+if not ls[-1][-1] or not ls[0][0]:
+    print('NO')
+else:
+    for i in range(1, n):
+        ls[i][0] = ls[i - 1][0]
+    for j in range(1, m):
+        ls[0][j] = ls[0][j - 1]
+
+    for i in range(1, n):
+        for j in range(1, m):
+            if not ls[i][j]:
+                continue
+                
+            ls[i][j] = ls[i - 1][j] or ls[i][j - 1]
+
+    if ls[-1][-1]:
+        print('YES')
+        print(n + m - 2)
+    else:
+        print('NO')
