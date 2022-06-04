bd64310cde1ef379d5f5b34eef2d9efc73ec0cb9
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:36:21 +0530

    Committing files

---
+++ CodeChef/PYTH 3.6/GREATESC/27725856.py
@@ -0,0 +1,49 @@
+def BFS():
+    queue = [s]
+
+    pred = [-1] * n
+
+    visited = [False] * n
+    visited[s] = True
+
+    dist = [float('inf')] * n
+    dist[s] = 0
+
+    while queue:
+        u = queue[0]
+        del queue[0]
+
+        for i in range(len(graph[u])):
+            if not visited[graph[u][i]]:
+                visited[graph[u][i]] = True
+                dist[graph[u][i]] = dist[u] + 1
+                pred[graph[u][i]] = u
+
+                queue.append(graph[u][i])
+
+                if graph[u][i] == d:
+                   return dist[graph[u][i]]
+
+    return 0
+
+n, m = map(int, input().split())
+
+if n != 1:
+    graph = [[] for _ in range(n)]
+    
+    for _ in range(m):
+        u, v = list(map(int, input().split()))
+        u -= 1
+        v -= 1
+    
+        if v != u:
+            graph[u].append(v)
+            graph[v].append(u)
+
+    s, d = map(int, input().split())
+    s -= 1
+    d -= 1
+    
+    print(BFS())
+else:
+    print(0)
