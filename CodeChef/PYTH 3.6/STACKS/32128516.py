3061e30332b87b0140a487f0a25d136bd7e0d384
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:36:52 +0530

    Committing files

---
+++ CodeChef/PYTH 3.6/STACKS/32128516.py
@@ -0,0 +1,25 @@
+import bisect
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    disks = []
+
+    for e in a:
+        i = bisect.bisect(disks, e)
+
+        if i == len(disks):
+            disks.append(e)
+            continue
+
+        if disks[i] == e:
+            i += 1
+
+        if i == len(a):
+            disks.append(e)
+            continue
+
+        disks[i] = e
+
+    print(len(disks), *disks)
