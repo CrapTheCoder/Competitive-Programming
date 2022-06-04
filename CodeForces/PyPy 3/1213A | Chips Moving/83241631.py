a01b7b4474275f1ffd0856493b65604d13fa10a7
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 11:15:11 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1213A | Chips Moving/83241631.py
@@ -0,0 +1,14 @@
+n = int(input())
+a = list(map(int, input().split()))
+ 
+m = max(a)
+ 
+o = e = 0
+ 
+for i in a:
+    if (m - i) % 2 == 0:
+        e += 1
+    else:
+        o += 1
+ 
+print(min(e, o))
