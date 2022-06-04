239ebb0afdcdae26b00b5c1965a2140fd43912df
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:41:22 +0530

    Committing files

---
+++ CodeChef/C++14/AODLS004/39839927.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long
+
+signed main(){
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	int a; cin >> a;
+	int b; cin >> b;
+	cout << a * b << endl;
+}
