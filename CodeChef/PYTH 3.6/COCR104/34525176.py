d433cf3ffa0dc54c903947fccebb53aba5c3d742
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:43:09 +0530

    Committing files

---
+++ CodeChef/PYTH 3.6/COCR104/34525176.py
@@ -0,0 +1,23 @@
+for _ in range(int(input())):
+    a, b, l, r = map(int, input().split())
+    
+    if a + b == 0:
+        print(0)
+        continue
+
+    x = r // (a + b)
+    rc = x * 2
+    if x * (a + b) + a <= r:
+        rc += 1
+
+    if l == 0:
+        print(rc)
+        continue
+
+    l -= 1
+    y = l // (a + b)
+    lc = y * 2
+    if y * (a + b) + a <= l:
+        lc += 1
+
+    print(rc - lc)
