8743cab24f67a0ba617ce6df6264147e2547c5f3
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 11:15:50 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1360B | Honest Coach/81216999.py
@@ -0,0 +1,11 @@
+for _ in range(int(input())):
+    n = int(input())
+    a = sorted(map(int, input().split()))
+ 
+    mini = float('inf')
+ 
+    for i in range(1, n):
+        if a[i] - a[i-1] < mini:
+            mini = a[i] - a[i-1]
+ 
+    print(mini)
