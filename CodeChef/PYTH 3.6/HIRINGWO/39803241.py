5eab8b6d0f994859dc47ab9e4900c97d5189eb42
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:53:34 +0530

    Committing files

---
+++ CodeChef/PYTH 3.6/HIRINGWO/39803241.py
@@ -0,0 +1,36 @@
+def solve(i, a, pfs):
+    if i == len(pfs):
+        return sum(a)
+
+    mini = float('inf')
+    for j in range(k):
+        a[j] *= pfs[i]
+        mini = min(mini, solve(i+1, a, pfs))
+        a[j] //= pfs[i]
+
+    return mini
+
+for _ in range(int(input())):
+    k, x = map(int, input().split())
+    ox = x
+
+    pfs = []
+
+    for i in range(2, int(x ** 0.5) + 1):
+        c = 0
+        while x % i == 0:
+            x //= i
+            c += 1
+
+        if c > 0:
+            pfs.append(i ** c)
+
+    if x > 1:
+        pfs.append(x)
+
+    if len(pfs) <= k:
+        print(sum(pfs) + k - len(pfs))
+        continue
+
+    a = [1] * k
+    print(solve(0, a, pfs))
