fbc73910a5fb44aab8363f90444f3f0523b22ea4
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 11:00:35 +0530

    Committing files

---
+++ CodeChef/PYTH 3.5/C001/19137099.py
@@ -0,0 +1,17 @@
+n=int(input())
+s=""
+l=[]
+for i in range(n):
+    x=list(input().split())
+    a=int(x[0])
+    if(a==1):
+        l.append(s)
+        s=s+x[1]
+    elif(a==2):
+        l.append(s)
+        s=s[0:len(s)-int(x[1])]
+    elif(a==3):
+        print(s[int(x[1])-1])
+    else:
+        s=l[len(l)-1]
+        l.pop(len(l)-1)
