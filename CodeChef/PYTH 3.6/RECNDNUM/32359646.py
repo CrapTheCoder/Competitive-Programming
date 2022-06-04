5ac8652e972ebc79a7c19b9b23450537335964da
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:44:00 +0530

    Committing files

---
+++ CodeChef/PYTH 3.6/RECNDNUM/32359646.py
@@ -0,0 +1,22 @@
+MOD = 1000000007
+
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+
+    if n == 0:
+        print(k * (k - 1) % MOD)
+        continue
+
+    if k == 0:
+        print(0)
+        continue
+
+    start = n ** 2
+    skip = n * 2
+
+    ans = start + skip * (k // 2)
+
+    k = -(-k // 2)
+    ans += k * (k - 1)
+
+    print(ans % MOD)
