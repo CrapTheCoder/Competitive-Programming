89811870410f80eb3455f0fa59cf85be7a66a27d
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:49:04 +0530

    Committing files

---
+++ CodeChef/PYTH 3.6/JCWC05/25947466.py
@@ -0,0 +1,8 @@
+for _ in range(int(input())):
+    n = int(input())
+    
+    if n > 4: print(1)
+    if n == 1: print(3/7)
+    if n == 2: print(4/7)
+    if n == 3: print(5/7)
+    if n == 4: print(6/7)
