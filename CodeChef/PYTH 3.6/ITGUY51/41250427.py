9ecf82df4f0678fe4f40d57ab2b3f919d369f895
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:39:56 +0530

    Committing files

---
+++ CodeChef/PYTH 3.6/ITGUY51/41250427.py
@@ -0,0 +1,12 @@
+for _ in range(int(input())):
+    n = int(input())
+
+    j = 1
+    for i in range(1, n+1):
+        l = []
+
+        for _ in range(1, n+1):
+            l.append(j)
+            j += 2
+
+        print(*l, sep='')
