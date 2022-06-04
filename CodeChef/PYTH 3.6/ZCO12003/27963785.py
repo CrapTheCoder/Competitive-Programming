7a75571d3345b3f4a11b6ad031e2bb4ff7ca1107
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:51:41 +0530

    Committing files

---
+++ CodeChef/PYTH 3.6/ZCO12003/27963785.py
@@ -0,0 +1,57 @@
+def solve(s, op, cl):
+    if op not in s:
+        return 0
+    
+    st = []
+    le = mle = 0
+
+    for c in s:
+        if c == op:
+            st.append(c)
+        if c == cl:
+            st.pop()
+
+        le += 1
+
+        if not st:
+            if le > mle:
+                mle = le
+
+            le = 0
+
+    return mle
+
+
+def alt_solve(s):
+    pre = []
+    dep = []
+    ma = 0
+
+    for c in s:
+        if c == ')' or c == ']':
+            pre.pop()
+            dep.pop()
+            continue
+
+        if not pre:
+            pre.append(c)
+            dep.append(1)
+
+        elif c != pre[-1]:
+            pre.append(c)
+            dep.append(dep[-1] + 1)
+
+        else:
+            pre.append(c)
+            dep.append(dep[-1])
+
+        if dep:
+            ma = max(ma, dep[-1])
+
+    return ma
+
+n = int(input())
+x = '()[]'
+s = ''.join(x[i - 1] for i in list(map(int, input().split())))
+
+print(alt_solve(s), solve(s, '(', ')'), solve(s, '[', ']'))
