5a3893998979762573ffa8d47e0a7dd1648ecb16
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:55:05 +0530

    Committing files

---
+++ CodeChef/C++14/HLPMIL/29350831.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef int integer;
+typedef long long int ll;
+
+#define int ll
+#define endl '\n'
+
+#define MOD 1000000007
+
+void multiply(vector<int> a[], vector<int> b[], vector<int> x[], int r1, int c1, int c2){
+    vector<int> c[r1];
+
+    for(int i = 0; i < 4; i++)
+        c[i].resize(c2);
+
+    for(int i = 0; i < r1; i++)
+        for(int j = 0; j < c2; j++)
+            for(int k = 0; k < c1; k++)
+                c[i][j] = (c[i][j] + (a[i][k] * b[k][j]) % MOD) % MOD;
+
+    for(int i = 0; i < r1; i++)
+        for(int j = 0; j < c2; j++)
+            x[i][j] = c[i][j];
+}
+
+void power(vector<int> x[], int n){
+    vector<int> res[4] = {
+                            {1, 0, 0, 0},
+                            {0, 1, 0, 0},
+                            {0, 0, 1, 0},
+                            {0, 0, 0, 1}
+                         };
+
+    while(n > 0){
+        if(n % 2)
+            multiply(res, x, res, 4, 4, 4);
+
+        n /= 2;
+        multiply(x, x, x, 4, 4, 4);
+    }
+
+    for(int i = 0; i < 4; i++)
+        for(int j = 0; j < 4; j++)
+            x[i][j] = res[i][j];
+}
+
+integer main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    int t; cin >> t;
+
+    while(t--){
+        int n; cin >> n;
+
+        vector<int> magic[4] = {
+                                {36, 0, 0, 0},
+                                {1, 36, 0, 0},
+                                {0, 36, 36, 0},
+                                {36, 0, 0, 1}
+                               };
+
+        power(magic, n);
+
+        int ans = magic[0][0] % MOD;
+        ans = (ans + ((magic[1][0] % MOD) * 36) % MOD) % MOD;
+        ans = (ans + ((magic[2][0] % MOD) * 36) % MOD) % MOD;
+        ans = (ans + magic[3][0] % MOD) % MOD;
+
+        cout << ans << endl;
+    }
+}
