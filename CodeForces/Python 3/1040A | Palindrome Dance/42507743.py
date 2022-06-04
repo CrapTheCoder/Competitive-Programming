4eb408a5ec4faabea2ff45dee4817353607dd77e
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 11:23:11 +0530

    Committing files

---
+++ CodeForces/Python 3/1040A | Palindrome Dance/42507743.py
@@ -0,0 +1,23 @@
+import math
+ 
+n, a, b = map(int, input().split())
+arr = list(map(int, input().split()))
+ 
+money = 0
+ 
+for i in range(math.ceil(n / 2)):
+    x, y = arr[i] == 2, arr[n - i - 1] == 2
+ 
+    if x and y:
+        z = i == n - i - 1
+        money += min(a, b) * (2 - z)
+        continue
+    if x:
+        money += [a, b][arr[n - i - 1]]
+    if y:
+        money += [a, b][arr[i]]
+    if (not x and not y) and arr[i] != arr[n - i - 1]:
+        print(-1)
+        break
+else:
+    print(money)
