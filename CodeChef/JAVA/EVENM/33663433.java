e8c7c3beb73b0c83832c5915a28c4758b827e378
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:43:21 +0530

    Committing files

---
+++ CodeChef/JAVA/EVENM/33663433.java
@@ -0,0 +1,286 @@
+import java.io.*;
+import java.util.Arrays;
+import java.lang.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        new Solver().solve();
+    }
+}
+
+class Solver {
+    final Helper hp;
+
+    Solver() {
+        hp = new Helper();
+        hp.initIO(System.in, System.out);
+    }
+
+    public void solve() throws Exception {
+        int tcs = TESTCASES ? hp.nextInt() : 1;
+
+        for(int tcn = 0; tcn < tcs; tcn++)
+            solve(tcn);
+
+        hp.flush();
+    }
+
+    boolean TESTCASES = true;
+
+    public void solve(int tcn) throws Exception {
+        int n = hp.nextInt();
+
+        int[][] mat = new int[n][n];
+
+        int o = 1;
+        int e = 2;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i + j) % 2 == 0) {
+                    mat[i][j] = o; o += 2;
+                } else {
+                    mat[i][j] = e; e += 2;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                hp.printsp(mat[i][j]);
+            hp.println();
+        }
+    }
+
+    public boolean isValid(int[][] mat, int n){
+        for(int r = 0; r < n; r++) for(int c = 0; c < n; c++) for(int a = 0; a < n - Math.max(r, c); a++){
+            if((mat[r][c] + mat[r+a][c+a]) % 2 != 0) return false;
+            if((mat[r+a][c] + mat[r][c+a]) % 2 != 0) return false;
+        }
+
+        return true;
+    }
+}
+
+class Helper {
+    public long[] getLongArray(int size) throws Exception {
+        long[] ar = new long[size];
+        for (int i = 0; i < size; ++i) ar[i] = nextLong();
+        return ar;
+    }
+
+    public int[] getIntArray(int size) throws Exception {
+        int[] ar = new int[size];
+        for (int i = 0; i < size; ++i) ar[i] = nextInt();
+        return ar;
+    }
+
+    public String[] getStringArray(int size) throws Exception {
+        String[] ar = new String[size];
+        for (int i = 0; i < size; ++i) ar[i] = next();
+        return ar;
+    }
+
+    public String joinElements(long... ar) {
+        StringBuilder sb = new StringBuilder();
+        for (long itr : ar) sb.append(itr).append(" ");
+        return sb.toString().trim();
+    }
+
+
+    public String joinElements(int... ar) {
+        StringBuilder sb = new StringBuilder();
+        for (int itr : ar) sb.append(itr).append(" ");
+        return sb.toString().trim();
+    }
+
+    public String joinElements(String... ar) {
+        StringBuilder sb = new StringBuilder();
+        for (String itr : ar) sb.append(itr).append(" ");
+        return sb.toString().trim();
+    }
+
+    public String joinElements(Object... ar) {
+        StringBuilder sb = new StringBuilder();
+        for (Object itr : ar) sb.append(itr).append(" ");
+        return sb.toString().trim();
+    }
+
+    public long gcd(long a, long b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
+    public int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
+    public long lcm(long a, long b) {
+        return a * b / gcd(a, b);
+    }
+
+    public int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+
+    public long max(long... ar) {
+        long ret = ar[0];
+        for (long itr : ar) ret = Math.max(ret, itr);
+        return ret;
+    }
+
+    public int max(int... ar) {
+        int ret = ar[0];
+        for (int itr : ar) ret = Math.max(ret, itr);
+        return ret;
+    }
+
+    public long min(long... ar) {
+        long ret = ar[0];
+        for (long itr : ar) ret = Math.min(ret, itr);
+        return ret;
+    }
+
+    public int min(int... ar) {
+        int ret = ar[0];
+        for (int itr : ar) ret = Math.min(ret, itr);
+        return ret;
+    }
+
+
+    public long sum(long... ar) {
+        long sum = 0;
+        for (long itr : ar) sum += itr;
+        return sum;
+    }
+
+    public long sum(int... ar) {
+        long sum = 0;
+        for (int itr : ar) sum += itr;
+        return sum;
+    }
+
+    public void reverse(int[] ar) {
+        int r;
+        for (int i = 0; i < ar.length; ++i) {
+            r = ar.length - 1 - i;
+            if (r > i) {
+                ar[i] ^= ar[r];
+                ar[r] ^= ar[i];
+                ar[i] ^= ar[r];
+            }
+        }
+    }
+
+    public void reverse(long[] ar) {
+        int r;
+        for (int i = 0; i < ar.length; ++i) {
+            r = ar.length - 1 - i;
+            if (r > i) {
+                ar[i] ^= ar[r];
+                ar[r] ^= ar[i];
+                ar[i] ^= ar[r];
+            }
+        }
+    }
+
+    public long pow(long base, long exp, long MOD) {
+        base %= MOD;
+        long ret = 1;
+        while (exp > 0) {
+            if ((exp & 1) == 1) ret = ret * base % MOD;
+            base = base * base % MOD;
+            exp >>= 1;
+        }
+        return ret;
+    }
+
+
+    static final int BUFFER_SIZE = 1 << 20;
+    static byte[] buf;
+    static int index, total;
+    static InputStream in;
+    static BufferedWriter bw;
+
+
+    public void initIO(InputStream is, OutputStream os) {
+        try {
+            in = is;
+            bw = new BufferedWriter(new OutputStreamWriter(os));
+            buf = new byte[BUFFER_SIZE];
+        } catch (Exception ignored) {
+        }
+    }
+
+    public void initIO(String inputFile, String outputFile) {
+        try {
+            in = new FileInputStream(inputFile);
+            bw = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(outputFile)));
+            buf = new byte[BUFFER_SIZE];
+        } catch (Exception ignored) {
+        }
+    }
+
+    private int scan() throws Exception {
+        if (index >= total) {
+            index = 0;
+            total = in.read(buf);
+            if (total <= 0)
+                return -1;
+        }
+        return buf[index++];
+    }
+
+    public String next() throws Exception {
+        int c = scan();
+        while (c <= 32) c = scan();
+        StringBuilder sb = new StringBuilder();
+        for (; c > 32; c = scan())
+            sb.append((char) c);
+        return sb.toString();
+    }
+
+    public int nextInt() throws Exception {
+        int c = scan(), val = 0;
+        while (c <= 32) c = scan();
+        boolean neg = c == '-';
+        if (c == '-' || c == '+')
+            c = scan();
+        for (; c >= '0' && c <= '9'; c = scan())
+            val = (val << 3) + (val << 1) + (c & 15);
+        return neg ? -val : val;
+    }
+
+    public long nextLong() throws Exception {
+        int c = scan(), val = 0;
+        while (c <= 32) c = scan();
+        boolean neg = c == '-';
+        if (c == '-' || c == '+')
+            c = scan();
+        for (; c >= '0' && c <= '9'; c = scan())
+            val = (val << 3) + (val << 1) + (c & 15);
+        return neg ? -val : val;
+    }
+
+    public void print(Object a) throws Exception {
+        bw.write(a.toString());
+    }
+
+    public void printsp(Object a) throws Exception {
+        print(a);
+        print(" ");
+    }
+
+    public void println() throws Exception {
+        bw.write("\n");
+    }
+
+    public void println(Object a) throws Exception {
+        print(a);
+        println();
+    }
+
+    public void flush() throws Exception {
+        bw.flush();
+    }
+}
