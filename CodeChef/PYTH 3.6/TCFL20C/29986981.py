86940518017e120e70904f9192b1209c3ece4b0c
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:45:04 +0530

    Committing files

---
+++ CodeChef/PYTH 3.6/TCFL20C/29986981.py
@@ -0,0 +1,16 @@
+MOD = 10 ** 9 + 7
+
+for _ in range(int(input())):
+    n = input().strip()
+
+    f = 1
+
+    for i in n:
+        i = int(i)
+
+        if i == 7 or i == 9:
+            f *= 4
+        else:
+            f *= 3
+
+    print(f % MOD)
