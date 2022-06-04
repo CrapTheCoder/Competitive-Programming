433326b82ee2aea87d61b47b49f847982f47d3f0
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:50:39 +0530

    Committing files

---
+++ CodeChef/C++14/INOI2002/39819637.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long
+
+#define MOD 1000000007
+#define MAXN 1000013
+
+int c1[MAXN];
+int c2[MAXN];
+int dp[MAXN * 72];
+
+int hasher(int a, int b, int c) {
+    int base = min({a, b, c});
+    a -= base; b -= base; c -= base;
+    return base * 67 + a + b * 4 + c * 16;
+}
+
+int solve(int a, int b, int c) {
+    if (a < 0 || b < 0 || c < 0)
+        return 0;
+
+    int h = hasher(a, b, c);
+
+    if (dp[h] >= 0)
+        return dp[h];
+
+    int s = 0;
+    int m = max({a, b, c});
+
+    if (a == b && b == c)
+        s += solve(a-1, b-1, c-1);
+
+    if (a == m) {
+        s += solve(a-3, b, c);
+        if (a == b)
+            s += solve(a-2, b-2, c);
+
+    } else if (b == m) {
+        s += solve(a, b-3, c);
+        if (b == c)
+            s += solve(a, b-2, c-2);
+
+    } else {
+        s += solve(a, b, c-3);
+    }
+
+    return dp[h] = s % MOD;
+}
+
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    c1[0] = 1;
+    c2[0] = c2[2] = 1;
+
+    fill(dp, dp + (MAXN * 72), -1);
+    dp[hasher(0, 0, 0)] = 1;
+
+    for (int i = 3; i < MAXN; i++) {
+        c1[i] = i%3 == 0 ? 1 : 0;
+        c2[i] = (c2[i-2] + c2[i-3]) % MOD;
+    }
+
+    int t; cin >> t;
+    while (t--) {
+        int k, n; cin >> k >> n;
+
+        if (k == 1) cout << c1[n];
+        else if (k == 2) cout << c2[n];
+        else cout << solve(n, n, n);
+
+        cout << endl;
+    }
+}
