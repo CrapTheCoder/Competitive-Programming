2ca22d1788c8d9893f3997c0dba9163ccd7182b5
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:48:53 +0530

    Committing files

---
+++ CodeChef/PYTH 3.6/ARROTATE/26088862.py
@@ -0,0 +1,6 @@
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+
+    print(*[i for i in a if i not in b] + b)
