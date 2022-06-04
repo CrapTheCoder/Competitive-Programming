590a1d846e7248b6dc670032a9fb9909212820a8
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:46:35 +0530

    Committing files

---
+++ CodeChef/C++14/MINIAND/28862811.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t; cin >> t;
+
+    while(t--){
+        int n; cin >> n;
+        int p[n+1]; p[0] = 0;
+
+        for(int i = 1; i <= n; i++){
+            int v; cin >> v;
+
+            p[i] = p[i - 1];
+
+            if(!(v & 1))
+                p[i]++;
+        }
+
+        int q; cin >> q;
+
+        while(q--){
+            int l, r; cin >> l >> r;
+
+            if(!(p[r] - p[l - 1]))
+                cout << "ODD";
+            else
+                cout << "EVEN";
+
+            cout << endl;
+        }
+    }
+}
