b51ee789062fcf9f447169c4e40485a1631a13cd
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:39:55 +0530

    Committing files

---
+++ CodeChef/PYTH 3.6/ITGUY52/41250538.py
@@ -0,0 +1,12 @@
+for _ in range(int(input())):
+    n = int(input())
+
+    j = 1
+    for i in range(1, n+1):
+        l = []
+
+        for _ in range(1, n+1):
+            l.append(bin(j)[2:])
+            j += 1
+
+        print(*l, sep=' ')
