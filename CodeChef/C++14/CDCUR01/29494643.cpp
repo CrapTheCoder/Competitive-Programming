e92cb57da694583b7a3234a5887724bc7bd59f8b
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:45:41 +0530

    Committing files

---
+++ CodeChef/C++14/CDCUR01/29494643.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef int integer;
+typedef long long int ll;
+
+#define int ll
+#define endl '\n'
+
+#define INF numeric_limits<float>::infinity()
+#define MOD 1000000007
+
+integer main(){
+    int tests; cin >> tests;
+
+    while(tests--){
+        int a[3];
+        cin >> a[0] >> a[1] >> a[2];
+
+        sort(a, a + 3);
+
+        a[2] -= a[1];
+        a[1] = 0;
+
+        a[2] -= a[0];
+        a[0] = 0;
+
+        if(a[2] > 3)
+            cout << "No" << endl;
+        else
+            cout << "Yes" << endl;
+    }
+}
