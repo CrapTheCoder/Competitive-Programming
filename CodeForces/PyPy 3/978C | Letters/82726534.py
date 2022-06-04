6296706315286896f5d3a13b77d60d36d317e311
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 11:15:21 +0530

    Committing files

---
+++ CodeForces/PyPy 3/978C | Letters/82726534.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+ 
+p = [0]
+ 
+for i in a:
+    p.append(p[-1] + i)
+ 
+cur = 1
+ 
+for i in b:
+    while i > p[cur]:
+        cur += 1
+ 
+    print(cur, i - p[cur - 1])
