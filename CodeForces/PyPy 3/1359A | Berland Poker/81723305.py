8badd569e6368ae829d76308086764ec85dbf7ed
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 11:15:40 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1359A | Berland Poker/81723305.py
@@ -0,0 +1,9 @@
+for _ in range(int(input())):
+    n, m, k = map(int, input().split())
+    x = n // k
+ 
+    if x >= m:
+        print(m)
+        continue
+ 
+    print(x - -(-(m - x) // (k - 1)))
