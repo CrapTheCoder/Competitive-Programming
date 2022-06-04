7621b8ca84643aad43e04b4bc98dc157af20bef1
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:40:08 +0530

    Committing files

---
+++ CodeChef/C++14/CPCEJC3/41162973.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    cin.ignore();
+
+    string s;
+    while (cin >> s) {
+        reverse(s.begin(), s.end());
+
+        for (int i = 0; i < s.size(); i++)
+            s[i] = tolower(s[i]);
+
+        cout << s << endl;
+    }
+}
