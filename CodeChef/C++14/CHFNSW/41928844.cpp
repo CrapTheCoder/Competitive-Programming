4f314c42e5f4eb8ba14a15e35b2af2ce515d607e
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-04 10:39:12 +0530

    Committing files

---
+++ CodeChef/C++14/CHFNSW/41928844.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define INF INT_MAX
+
+signed main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    int t; cin >> t;
+
+    while (t--) {
+        int n, m; cin >> n >> m;
+        cout << n * m / 2 << endl;
+    }
+}
