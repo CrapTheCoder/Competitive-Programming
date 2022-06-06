e9d52319a1905a12e3250bb265bccbb6ba394f94
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 01:20:29 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/811A | Vladik and Courtesy/149826313.cpp
@@ -0,0 +1,19 @@
+bits/stdc++.h>
+namespace std;
+
+int long long
+endl '\n'
+
+MOD = 1e9 + 7;
+INF = LLONG_MAX >> 1;
+
+main() {
+sync_with_stdio(false); cin.tie(NULL);
+
+b; cin >> a >> b;
+
+int cur = 1; ; cur++) {
+cur % 2) ? (a -= cur) : (b -= cur);
+(a < 0) { cout << "Vladik" << endl; break; }
+(b < 0) { cout << "Valera" << endl; break; }
+
