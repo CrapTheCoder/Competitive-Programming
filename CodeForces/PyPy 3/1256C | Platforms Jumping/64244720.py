23d4532c5fd1c95925144c94e5ab0b211867cf1a
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 03:18:16 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1256C | Platforms Jumping/64244720.py
@@ -0,0 +1,25 @@
+n, m, d = map(int, input().split())
+a = list(map(int, input().split()))
+ 
+l = [0] * (n * 2)
+ci = d - 1
+ 
+for i in range(m):
+    l[ci:ci+a[i]] = [i + 1] * a[i]
+ 
+    if ci >= n:
+        for _ in range(ci - n + 1):
+            l.remove(0)
+ 
+        ci = n + a[i] - 1
+    else:
+        ci += a[i] - 1 + d
+ 
+for i in range(ci - n):
+    l.remove(0)
+ 
+if ci < n:
+    print('NO')
+else:
+    print('YES')
+    print(*l[:n])
