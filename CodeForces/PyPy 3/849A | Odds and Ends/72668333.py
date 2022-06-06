d361d601475ba2da9b15c04ab1464a530d3bf5b8
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 03:13:57 +0530

    Committing files

---
+++ CodeForces/PyPy 3/849A | Odds and Ends/72668333.py
@@ -0,0 +1,36 @@
+n = int(input())
+a = list(map(int, input().split()))
+ 
+flag = False
+ 
+i = 0
+c = 0
+ 
+while i < n:
+    if not a[i] % 2:
+        print('No')
+        break
+ 
+    j = i
+ 
+    last = -1
+ 
+    while j < n:
+        if a[j] % 2 and (i - j + 1) % 2:
+            last = j
+ 
+        j += 1
+ 
+    if last == -1:
+        print('No')
+        break
+ 
+    i = last + 1
+ 
+    c += 1
+ 
+else:
+    if c % 2:
+        print('Yes')
+    else:
+        print('No')
