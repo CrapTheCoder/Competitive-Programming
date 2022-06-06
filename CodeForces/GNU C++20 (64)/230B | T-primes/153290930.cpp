499a1534d50a24e6dac1cf4ae893c33a1d39c9e6
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 01:19:15 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/230B | T-primes/153290930.cpp
@@ -0,0 +1,36 @@
+bits/stdc++.h>
+namespace std;
+
+int long long
+endl '\n'
+
+MOD = 1e9 + 7;
+INF = LLONG_MAX >> 1;
+
+1e6 + 13;
+
+main() {
+sync_with_stdio(false); cin.tie(NULL);
+
+sieve[MAX];
+sieve, sieve + MAX, true);
+0] = sieve[1] = false;
+
+<int> s;
+
+int i = 2; i < MAX; i++) {
+(sieve[i]) {
+s.insert(i*i);
+for (int j = i * i; j < MAX; j += i)
+sieve[j] = false;
+
+
+
+cin >> n;
+(n--) {
+int x; cin >> x;
+(s.count(x))
+cout << "YES" << endl;
+else
+cout << "NO" << endl;
+
