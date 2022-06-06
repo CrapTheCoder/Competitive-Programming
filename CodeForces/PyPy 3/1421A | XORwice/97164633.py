f604955ab1100b8024e8df661b3c8de0a18f1843
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 02:32:38 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1421A | XORwice/97164633.py
@@ -0,0 +1,10 @@
+for _ in range(int(input())):
+a, b = map(int, input().split())
+
+xor = 0
+
+for i in range(max(len(bin(a)), len(bin(b)))):
+if (a // 2 ** i) % 2 == (b // 2 ** i) % 2 == 1:
+xor += 2 ** i
+
+print((a ^ xor) + (b ^ xor))
