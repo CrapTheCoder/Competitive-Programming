b7e9e6df8441cdc4a8406062b267a0de11ba9312
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 01:20:27 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/811A | Vladik and Courtesy/149826367.cpp
@@ -0,0 +1,11 @@
+bits/stdc++.h>
+namespace std;
+
+main() {
+b; cin >> a >> b;
+
+int cur = 1; ; cur++) {
+cur % 2) ? (a -= cur) : (b -= cur);
+(a < 0) { cout << "Vladik" << endl; break; }
+(b < 0) { cout << "Valera" << endl; break; }
+
