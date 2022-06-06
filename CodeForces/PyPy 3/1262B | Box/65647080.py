dae8633a3102cff25402f5c7bda149fd0af31c56
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 02:45:52 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1262B | Box/65647080.py
@@ -0,0 +1,49 @@
+from heapq import *
+
+for _ in  range(int(input())):
+n = int(input())
+a = list(map(int, input().split()))
+
+p = list(range(1, n + 1))
+
+u = [0] * n
+
+x = [a[0]]
+
+if a[0] == 1:
+heappop(p)
+else:
+u[a[0] - 1] = 1
+
+for i in range(1, n):
+if a[i] == a[i-1]:
+m = heappop(p)
+
+if m > a[i - 1]:
+print(-1)
+break
+
+while u[m - 1]:
+m = heappop(p)
+
+if m > a[i - 1]:
+print(-1)
+break
+
+else:
+u[m - 1] = 1
+x.append(m)
+continue
+
+break
+
+else:
+if a[i] < a[i - 1]:
+print(-1)
+break
+
+u[a[i] - 1] = 1
+x.append(a[i])
+
+else:
+print(*x)
