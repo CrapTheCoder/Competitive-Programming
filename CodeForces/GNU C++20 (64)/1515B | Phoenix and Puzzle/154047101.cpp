d9aba516b6f4aca0b25b3452c4d85eecc2318395
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 01:18:55 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1515B | Phoenix and Puzzle/154047101.cpp
@@ -0,0 +1,22 @@
+//Author: Shivank
+bits/stdc++.h>
+
+namespace std; using ll = long long int;
+tc int t; cin >> t; while(t--)
+
+() {
+cin >> n;
+= sqrt(n/2), b = sqrt(n/4);
+
+a*2 == n || b*b*4 == n)
+cout << "YES" << endl;
+
+cout << "NO" << endl;
+
+
+{
+ios_base::sync_with_stdio(0); cin.tie(nullptr);
+
+
+solve();
+
