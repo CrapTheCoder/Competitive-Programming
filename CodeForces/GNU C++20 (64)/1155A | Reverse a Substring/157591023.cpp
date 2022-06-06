795305cdf4f76ffe0fcbf76f3580c4d75be017d5
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 01:11:45 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1155A | Reverse a Substring/157591023.cpp
@@ -0,0 +1,31 @@
+bits/stdc++.h>
+namespace std;
+
+endl '\n'
+int long long
+
+MOD = 1e9 + 7;
+INF = LLONG_MAX >> 1;
+
+main() {
+sync_with_stdio(false); cin.tie(NULL);
+
+cin >> n;
+string s; cin >> s;
+
+m = s[0];
+mi = 0;
+
+int i = 1; i < n; i++) {
+(m > s[i]) {
+cout << "YES" << endl;
+cout << mi+1 << " " << i+1 << endl;
+return 0;
+
+else {
+m = s[i];
+mi = i;
+
+
+
+<< "NO" << endl;
