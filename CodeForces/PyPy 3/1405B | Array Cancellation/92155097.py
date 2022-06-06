d4bb0cc39753c53f5c906df8b5171c00c5a4e18a
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 03:07:30 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1405B | Array Cancellation/92155097.py
@@ -0,0 +1,14 @@
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+ 
+    s = 0
+ 
+    for i in range(n-1, -1, -1):
+        if a[i] < 0:
+            s += abs(a[i])
+ 
+        else:
+            s = max(s - a[i], 0)
+ 
+    print(s)
