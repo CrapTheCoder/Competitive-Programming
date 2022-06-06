507c3d9d5b680f4b9bcdaa930b15ce6c7f58bba5
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 01:10:13 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/579A | Raising Bacteria/159575249.cpp
@@ -0,0 +1,24 @@
+bits/stdc++.h>
+namespace std;
+
+endl '\n'
+int long long
+
+MOD = 1e9 + 7;
+INF = LLONG_MAX >> 1;
+
+main() {
+sync_with_stdio(false); cin.tie(NULL);
+
+cin >> n;
+count = 0;
+
+(n > 0) {
+while (n % 2 == 0)
+n /= 2;
+
+count++;
+/= 2;
+
+
+<< count << endl;
