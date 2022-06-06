57bfe16c84ba87a8e66188b4243d0f711f8bde6f
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 01:21:03 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/265B | Roadside Trees (Simplified Edition)/149255968.cpp
@@ -0,0 +1,14 @@
+bits/stdc++.h>
+namespace std;
+
+main() {
+cin >> n;
+
+time = 0, prev = 0;
+int i = 1; i <= n; i++) {
+int cur; cin >> cur;
+time += abs(cur - prev);
+prev = cur;
+
+
+<< time + n + (n-1) << endl;
