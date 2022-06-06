e8b33a452d299dfbf988bf4e02a129edc465ae5a
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 01:19:49 +0530

    Committing files

---
+++ CodeForces/PyPy 3-64/1305B | Kuroni and Simple Strings/151402802.py
@@ -0,0 +1,18 @@
+()
+
+
+[], []
+for i in range(n) if s[i] == '('][::-1]
+for i in range(n) if s[i] == ')']
+
+and cls and ops[-1] < cls[-1]:
+append(str(ops.pop() + 1))
+append(str(cls.pop() + 1))
+
++ x2) == 0:
+0)
+
+
+1)
+len(x1 + x2))
+(*x1 + x2[::-1])
