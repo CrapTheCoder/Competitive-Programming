c826b26d6e8e4394cb75b141de2e86fa06b47426
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 03:08:09 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1399C | Boats Competition/89063790.py
@@ -0,0 +1,21 @@
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    
+    d = {}
+    
+    for i in range(n):
+        for j in range(i+1, n):
+            cs = a[i] + a[j]
+            d[cs] = d.get(cs, set())
+            
+            if (i in d[cs]) or (j in d[cs]):
+                continue
+            
+            d[cs].add(i)
+            d[cs].add(j)
+    
+    if d:
+        print(max(len(d[i]) for i in d) // 2)
+    else:
+        print(0)
