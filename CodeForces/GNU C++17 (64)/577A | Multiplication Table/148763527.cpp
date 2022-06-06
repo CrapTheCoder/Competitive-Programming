a6c2c0f840788bdbe162ac7e1e0f4d3027d4641c
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 01:21:22 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/577A | Multiplication Table/148763527.cpp
@@ -0,0 +1,22 @@
+bits/stdc++.h>
+
+namespace std;
+ll long long
+
+{
+x;
+n >> x;
+
+= 0;
+int i = 1; i <= sqrt(x); i++) {
+(x % i == 0) {
+if (i > n || (x / i) > n)
+continue;
+
+c++;
+if (i != x / i)
+c++;
+
+
+
+<< c << endl;
