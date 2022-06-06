53a3d06991d6d03fb8c14f2f14109abf9d746c1d
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 02:34:05 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1409B | Minimum Product/91816502.py
@@ -0,0 +1,12 @@
+def solve(a, b, x, y, n):
+na = max(x, a - n)
+n -= a - na
+
+b = max(y, b - n)
+a = na
+
+return a * b
+
+for _ in range(int(input())):
+a, b, x, y, n = map(int, input().split())
+print(min(solve(a, b, x, y, n), solve(b, a, y, x, n)))
