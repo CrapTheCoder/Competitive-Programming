954c8c1308381fa99af3a4a9e54d24f9d1f18b56
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 03:06:23 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1165B | Polycarp Training/97030628.py
@@ -0,0 +1,16 @@
+n = int(input())
+a = sorted(map(int, input().split()))
+c = 1
+ 
+i = 0
+while True:
+    while i < n and a[i] < c:
+        i += 1
+ 
+    if i == n:
+        break
+ 
+    i += 1
+    c += 1
+ 
+print(c-1)
