21d5590e0b3c2a53611c2d2610c47e76c8d2e7b0
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 01:13:22 +0530

    Committing files

---
+++ CodeForces/PyPy 3-64/1480C | Searching Local Minimum/155814885.py
@@ -0,0 +1,33 @@
+i):
+] != -1:
+return a[i]
+
+'?', i, flush=True)
+int(input())
+a[i]
+
+
+i):
+query(i-1) < query(i):
+return True
+
+query(i) > query(i+1):
+return False
+
+'!', i, flush=True)
+
+
+
+input())
+(n+2)
+1] = float('inf')
+
+n
+
+r:
+l + r) // 2
+
+check(m):
+= m-1
+
+= m+1
