01b4a8ee564de0f73966bae0ada5b4e86640cc85
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 01:20:13 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1478B | Nezzar and Lucky Number/150042783.cpp
@@ -0,0 +1,32 @@
+bits/stdc++.h>
+namespace std;
+
+int long long
+endl '\n'
+
+MOD = 1e9 + 7;
+INF = LLONG_MAX >> 1;
+
+(int n, int d) {
+(n >= 0) {
+-= d;
+(n % 10 == 0 || n+d>=10*d) {
+cout << "YES" << endl;
+return;
+
+
+
+<< "NO" << endl;
+
+
+main() {
+sync_with_stdio(false); cin.tie(NULL);
+
+; cin >> tc;
+(tc--) {
+int n, d; cin >> n >> d;
+while (n--) {
+int x; cin >> x;
+solve(x, d);
+
+
