e145d1e96d6bd7e8afd0869334083b6e29708d92
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 02:41:05 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1323A | Even Subset Sum Problem/72735622.py
@@ -0,0 +1,25 @@
+for _ in range(int(input())):
+n = int(input())
+a = list(map(int, input().split()))
+
+if n == 1:
+if a[0] % 2:
+print(-1)
+else:
+print(1)
+print(1)
+
+continue
+
+if a[0] % 2 == 0:
+print(1)
+print(1)
+continue
+
+if a[1] % 2 == 0:
+print(1)
+print(2)
+continue
+
+print(2)
+print(1, 2)
