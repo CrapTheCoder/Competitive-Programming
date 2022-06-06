7cb4279ec7d6d7f505284771004f161325481a9d
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 02:43:05 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1307B | Cow and Friend/71295596.py
@@ -0,0 +1,18 @@
+from math import sqrt, ceil
+
+def dist(x1, y1, x2, y2):
+return sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+for _ in range(int(input())):
+n, x = map(int, input().split())
+a = list(map(int, input().split()))
+
+m = max(a)
+
+if x < m:
+if x in a:
+print(1)
+else:
+print(2)
+else:
+print(ceil(x / m))
