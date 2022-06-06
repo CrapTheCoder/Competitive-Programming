a740f8533c9674d79263cafc9ad19f4e32f55db4
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 03:07:58 +0530

    Committing files

---
+++ CodeForces/Python 3/1391B | Fix You/89417025.py
@@ -0,0 +1,15 @@
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    
+    l = [input() for _ in range(n)]
+    c = 0
+    
+    for i in range(n-1):
+        if l[i][-1] != 'D':
+            c += 1
+    
+    for j in range(m-1):
+        if l[-1][j] != 'R':
+            c += 1
+    
+    print(c)
