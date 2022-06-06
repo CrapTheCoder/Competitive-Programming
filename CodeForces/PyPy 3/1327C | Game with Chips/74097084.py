8912072e01fd019210f7717909b9f9e88d30c474
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 02:39:36 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1327C | Game with Chips/74097084.py
@@ -0,0 +1,25 @@
+n, m, k = map(int, input().split())
+
+sx = sy = -1
+
+for _ in range(k):
+csx, csy = tuple(map(int, input().split()))
+sx = max(sx, csx)
+sy = max(sy, csy)
+
+ans = ''
+
+ans += 'U' * (sx-1)
+ans += 'L' * (sy-1)
+
+for i in range(n):
+if i % 2 == 0:
+ans += 'R' * (m-1)
+else:
+ans += 'L' * (m-1)
+
+if i != n-1:
+ans += 'D'
+
+print(len(ans))
+print(ans)
