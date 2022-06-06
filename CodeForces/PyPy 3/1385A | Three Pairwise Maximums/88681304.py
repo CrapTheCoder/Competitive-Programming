b20eb91c6532ded6dbaf471626b2340e27aec45c
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 03:08:41 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1385A | Three Pairwise Maximums/88681304.py
@@ -0,0 +1,31 @@
+for _ in range(int(input())):
+    x, y, z = sorted(map(int, input().split()))
+ 
+    if x == y == z:
+        print('YES')
+        print(x, y, z)
+        continue
+ 
+    if x == y and y < z:
+        print('NO')
+        continue
+ 
+    if x < y and y == z:
+        print('YES')
+        print(x, x, z)
+        continue
+ 
+    print('NO')
+ 
+"""
+ 
+x, y, z
+ 
+(a, b), (a, c), (b, c)
+ 
+x y z
+1 2 2
+ 
+ 
+ 
+"""
