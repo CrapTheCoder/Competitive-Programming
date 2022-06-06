a57f5ed8db161cc29f68cea8e632ef66fa4160ad
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 03:14:02 +0530

    Committing files

---
+++ CodeForces/Python 3/1003B | Binary String Constructing/72664794.py
@@ -0,0 +1,31 @@
+a, b, x = map(int, input().split())
+ 
+new = ''
+ 
+if a > b:
+    for i in range(x - 1):
+        if i % 2:
+            new += '1'
+            b -= 1
+        else:
+            new += '0'
+            a -= 1
+ 
+else:
+    for i in range(1, x):
+        if i % 2:
+            new += '1'
+            b -= 1
+        else:
+            new += '0'
+            a -= 1
+ 
+if new and new[-1] == '1':
+    new += '0' * a
+    new += '1' * b
+ 
+else:
+    new += '1' * b
+    new += '0' * a
+ 
+print(new)
