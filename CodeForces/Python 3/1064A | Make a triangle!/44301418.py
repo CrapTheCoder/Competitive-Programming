bb94262ce2479d37796d04d7b6bd228499d5c74c
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 02:47:21 +0530

    Committing files

---
+++ CodeForces/Python 3/1064A | Make a triangle!/44301418.py
@@ -0,0 +1,19 @@
+a, b, c = map(int, input().split())
+
+m = max(a, b, c)
+
+x = y = -1
+
+if m == a:
+x = b
+y = c
+
+if m == b:
+x = a
+y = c
+
+if m == c:
+x = a
+y = b
+
+print(max((m - (x + y)) + 1, 0))
