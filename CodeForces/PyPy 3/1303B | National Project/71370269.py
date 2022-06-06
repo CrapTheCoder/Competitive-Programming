a9535803559825c8c26806e8d90247c64455eaee
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 02:43:00 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1303B | National Project/71370269.py
@@ -0,0 +1,14 @@
+from math import ceil
+
+for _ in range(int(input())):
+n, g, b = map(int, input().split())
+
+if b <= g:
+print(n)
+continue
+
+x = ceil(n / 2)
+
+a = (g + b) * ((x - 1) // g) + (x - 1) % g + 1
+
+print(max(n, a))
