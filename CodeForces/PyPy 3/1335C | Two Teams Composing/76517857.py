ff0aae2472d2435627c24cfab4e33fed687875d7
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 02:38:32 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1335C | Two Teams Composing/76517857.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+for _ in range(int(input())):
+n = int(input())
+a = list(map(int, input().split()))
+
+c = sorted(Counter(a).values())
+
+maxi = 0
+
+for i in range(len(c)):
+maxi = max(maxi, min(c[i], i), min(c[i] - 1, i + 1))
+
+print(maxi)
