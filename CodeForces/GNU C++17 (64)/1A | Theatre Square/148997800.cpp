7d2cb80000bdfa9722c0f921756cde37b95bcb58
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 01:21:16 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1A | Theatre Square/148997800.cpp
@@ -0,0 +1,16 @@
+bits/stdc++.h>
+namespace std;
+
+int long long
+endl '\n'
+
+MOD = 1e9 + 7;
+INF = LLONG_MAX >> 1;
+
+main() {
+sync_with_stdio(false); cin.tie(NULL);
+
+m, a;
+n >> m >> a;
+
+<< ((n+a-1) / a) * ((m+a-1) / a) << endl;
