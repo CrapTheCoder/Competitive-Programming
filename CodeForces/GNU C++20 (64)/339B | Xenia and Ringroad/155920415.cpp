b32a2238cd639137a6db45d30fac5b294fb7e924
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 01:13:17 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/339B | Xenia and Ringroad/155920415.cpp
@@ -0,0 +1,23 @@
+bits/stdc++.h>
+namespace std;
+
+endl '\n'
+int long long
+
+MOD = 1e9 + 7;
+INF = LLONG_MAX >> 1;
+
+main() {
+sync_with_stdio(false); cin.tie(NULL);
+
+m;
+n >> m;
+
+cur = 0, t = 0;
+int i = 0; i < m; i++) {
+int x; cin >> x; x--;
++= (x - cur + n) % n;
+cur = x;
+
+
+<< t << endl;
