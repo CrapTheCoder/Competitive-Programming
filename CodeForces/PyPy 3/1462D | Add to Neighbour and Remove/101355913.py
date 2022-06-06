4e8bce4217711ea918dfee32ea4d97612fa8d85f
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 03:05:30 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1462D | Add to Neighbour and Remove/101355913.py
@@ -0,0 +1,30 @@
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+ 
+    maxi = 1
+    s = 0
+ 
+    for i in range(n):
+        s += a[i]
+ 
+        t = 1
+        cs = 0
+ 
+        for j in range(i+1, n):
+            cs += a[j]
+ 
+            if cs == s:
+                cs = 0
+                t += 1
+ 
+            elif cs > s:
+                break
+ 
+        else:
+            if cs != 0:
+                continue
+ 
+            maxi = max(maxi, t)
+ 
+    print(n - maxi)
