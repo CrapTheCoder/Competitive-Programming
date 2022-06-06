d1bc99cad27759e0e8e570ce522ba6956b31bf1d
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 02:32:18 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1440B | Sum of Medians/98694992.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+for _ in range(int(input())):
+k, n = map(int, input().split())
+a = deque(sorted(map(int, input().split())))
+s = 0
+
+while a:
+d = []
+
+for _ in range((k-1) // 2):
+d.append(a.popleft())
+
+for _ in range(k - (k-1) // 2):
+d.append(a.pop())
+
+d.sort()
+s += d[(len(d) + 1) // 2 - 1]
+
+print(s)
