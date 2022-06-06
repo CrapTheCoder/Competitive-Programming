10f1a5afa88c70695739aac005106726e4ad82c3
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 02:43:07 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1307A | Cow and Haybales/71289494.py
@@ -0,0 +1,15 @@
+for _ in range(int(input())):
+n, d = map(int, input().split())
+a = list(map(int, input().split()))
+
+for i in range(1, n):
+if d - (i * a[i]) >= 0:
+d -= i * a[i]
+a[0] += a[i]
+else:
+x = d // i
+
+a[0] += min(a[i], x)
+break
+
+print(a[0])
