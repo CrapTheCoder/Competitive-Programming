5d9eccebb03f4c4636b84d240ffab8d92d28683f
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 02:36:39 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1360G | A/B Matrix/81456695.py
@@ -0,0 +1,20 @@
+for _ in range(int(input())):
+n, m, a, b = map(int, input().split())
+
+if a * n != b * m:
+print('NO')
+continue
+
+g = [[0] * m for _ in range(n)]
+
+cur = 0
+
+for i in range(n):
+for _ in range(a):
+g[i][cur] = 1
+cur = (cur + 1) % m
+
+print('YES')
+
+for row in g:
+print(*row, sep='')
