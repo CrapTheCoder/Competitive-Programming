7a0fa75980459bbba6d6dadc909a6369f211e0db
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 02:39:33 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1321A | Contest for Robots/74264419.py
@@ -0,0 +1,23 @@
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+x1 = set(i for i in range(n) if a[i])
+x2 = set(i for i in range(n) if b[i])
+
+x = len(x1.difference(x2))
+
+if x == 0:
+print(-1)
+exit()
+
+s1 = sum(a)
+s2 = sum(b)
+
+ms = 1
+
+while s1 <= s2:
+s1 += x
+ms += 1
+
+print(ms)
