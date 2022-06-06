63d7ae170b0d4e8ad5a02c7b58e2e214bbb2299f
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 03:17:57 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1253C | Sweets Eating/65204893.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+a = sorted(map(int, input().split()))
+ 
+l = [a[0]]
+y = [0] * m
+y[0] = a[0]
+ 
+for i in range(1, m):
+    l.append(l[-1] + a[i])
+    y[i] = a[i]
+ 
+for i in range(m, n):
+    x = l[-1]
+ 
+    y[i % m] += a[i]
+ 
+    if i - m >= 0:
+        x += y[i % m]
+ 
+    l.append(x)
+ 
+print(*l)
+ 
