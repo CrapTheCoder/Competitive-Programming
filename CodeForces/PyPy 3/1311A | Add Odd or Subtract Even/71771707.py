f63832f22d93326a878d7877727d4e8235482796
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 03:15:06 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1311A | Add Odd or Subtract Even/71771707.py
@@ -0,0 +1,22 @@
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+ 
+    if a == b:
+        print(0)
+        continue
+ 
+    if a < b:
+        if (a - b) % 2 == 1:
+            print(1)
+            continue
+ 
+        if (a - b) % 2 == 0:
+            print(2)
+ 
+    if a > b:
+        if (a - b) % 2 == 0:
+            print(1)
+            continue
+ 
+        if (a - b) % 2 == 1:
+            print(2)
