d0decdf1c502112cd646b906f09e12f1c818fd79
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 03:10:12 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1269C | Long Beautiful Integer/80916731.py
@@ -0,0 +1,14 @@
+n, k = list(map(int, input().split()))
+ 
+o = s = input()
+s = s[:k]
+ 
+d1 = (s * (n // k + 1))[:n]
+d2 = (str(int(s) + 1) * (n // k + 1))[:n]
+ 
+print(n)
+ 
+if d1 >= o:
+    print(d1)
+else:
+    print(d2)
