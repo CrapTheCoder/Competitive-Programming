804ec8cca998c052b33ceab99a89cb375062eb16
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 03:13:22 +0530

    Committing files

---
+++ CodeForces/Python 3/1313C1 | Skyscrapers (easy version)/73402291.py
@@ -0,0 +1,27 @@
+n = int(input())
+a = list(map(int, input().split()))
+ 
+m = -1
+mb = []
+ 
+for mi in range(n):
+    b = [-1] * n
+    b[mi] = a[mi]
+ 
+    for i in range(mi-1, -1, -1):
+        if a[i] > b[i+1]:
+            b[i] = b[i+1]
+        else:
+            b[i] = a[i]
+ 
+    for i in range(mi+1, n):
+        if a[i] > b[i-1]:
+            b[i] = b[i-1]
+        else:
+            b[i] = a[i]
+ 
+    if m < sum(b):
+        m = sum(b)
+        mb = b
+ 
+print(*mb)
