7f819162268c6b609db2172582449027603243b5
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 02:31:58 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1462A | Favorite Sequence/101353147.py
@@ -0,0 +1,20 @@
+for _ in range(int(input())):
+n = int(input())
+a = list(map(int, input().split()))
+
+b = [0] * n
+
+l, r, i = 0, n-1, 0
+
+while l <= r:
+if not i % 2:
+b[i] = a[l]
+l += 1
+
+else:
+b[i] = a[r]
+r -= 1
+
+i += 1
+
+print(*b)
