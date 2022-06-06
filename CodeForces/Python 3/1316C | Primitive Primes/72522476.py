4e0db8baa946148a4271a67a1e0ba925dd04d055
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 02:41:20 +0530

    Committing files

---
+++ CodeForces/Python 3/1316C | Primitive Primes/72522476.py
@@ -0,0 +1,19 @@
+from sys import stdin
+
+input = stdin.readline
+
+n, m, p = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+i = j = 0
+
+for i in range(n):
+if a[i] % p != 0:
+break
+
+for j in range(m):
+if b[j] % p != 0:
+break
+
+print(i + j)
