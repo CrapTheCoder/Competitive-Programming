fd4fa661a30b6f48ed6b526af082095a7a79bf91
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 03:12:38 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1144A | Diverse Strings/74380925.py
@@ -0,0 +1,9 @@
+for _ in range(int(input())):
+    s = sorted(input())
+ 
+    l = [ord(i) for i in s]
+ 
+    if all(l[i] - l[i-1] == 1 for i in range(1, len(s))):
+        print('Yes')
+    else:
+        print('No')
