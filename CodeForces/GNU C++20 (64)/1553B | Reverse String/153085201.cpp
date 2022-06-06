09472bc13bb671e17f090335bed220653fea805b
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 01:19:23 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1553B | Reverse String/153085201.cpp
@@ -0,0 +1,33 @@
+bits/stdc++.h>
+namespace std;
+
+main() {
+; cin >> tc;
+(tc--) {
+string s, t; cin >> s >> t;
+int n = s.size(), m = t.size();
+
+bool poss = false;
+for (int i = 0; i < n; i++) {
+for (int j = i; j < n; j++) {
+string res;
+
+// Left
+for (int l = i; l < j; l++)
+res += s[l];
+
+// Right
+for (int l = j; l >= j-(m-(j-i+1)); l--) {
+if (l < 0) goto skip;
+res += s[l];
+}
+
+if (res == t)
+poss = true;
+
+skip:;
+}
+
+
+cout << (poss ? "YES" : "NO") << endl;
+
