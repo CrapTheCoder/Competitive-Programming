4907aecbc4e46cec4c186b1c33c95732abbc87b3
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 03:06:53 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1082A | Vasya and Book/95960834.py
@@ -0,0 +1,18 @@
+for _ in range(int(input())):
+    n, x, y, d = map(int, input().split())
+ 
+    if abs(x - y) % d == 0:
+        print(abs(x - y) // d)
+    else:
+        mini = float('inf')
+ 
+        if (y-1) % d == 0:
+            mini = min(mini, (y-1) // d + -(-(x-1) // d))
+ 
+        if (n-y) % d == 0:
+            mini = min(mini, (n-y) // d + -(-(n-x) // d))
+ 
+        if mini == float('inf'):
+            print(-1)
+        else:
+            print(mini)
