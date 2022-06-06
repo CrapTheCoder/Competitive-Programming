5622beab941afcde5a997acf9570dbbdae689322
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 03:15:04 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1311B | WeirdSort/71775185.py
@@ -0,0 +1,22 @@
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+    p = list(map(lambda x: int(x) - 1, input().split()))
+ 
+    b = sorted(a)
+    c = a.copy()
+ 
+    while True:
+        for i in p:
+            if a[i] > a[i + 1]:
+                a[i], a[i + 1] = a[i + 1], a[i]
+ 
+        if a == b:
+            print('YES')
+            break
+ 
+        if a == c:
+            print('NO')
+            break
+ 
+        c = a.copy()
