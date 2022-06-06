9c9ed009dc89d1955d3af9d0247f622da477429c
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 02:33:31 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1426D | Non-zero Segments/94143144.py
@@ -0,0 +1,19 @@
+n = int(input())
+a = list(map(int, input().split()))
+
+d = {0}
+cur_sum = 0
+
+total = 0
+
+for i in range(n):
+cur_sum += a[i]
+
+if cur_sum in d:
+total += 1
+cur_sum = a[i]
+d = {0}
+
+d.add(cur_sum)
+
+print(total)
