97a6318421e0fe82c56d7153782c0623de88cdc6
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 01:18:35 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1633D | Make Them Equal/155745077.cpp
@@ -0,0 +1,52 @@
+bits/stdc++.h>
+namespace std;
+
+endl '\n'
+int long long
+
+MOD = 1e9 + 7;
+INF = LLONG_MAX >> 1;
+
+
+], c[1013], cost[4013];
+1013][15013];
+
+int i, int r) {
+< 0) return -INF;
+== n) return 0;
+
+[i][r] != -1)
+return dp[i][r];
+
+dp[i][r] = max(solve(i+1, r - cost[b[i]]) + c[i], solve(i+1, r));
+
+
+main() {
+sync_with_stdio(false); cin.tie(NULL);
+
+cost, cost+4013, INF); cost[1] = 0;
+int i = 1; i < 2000; i++)
+for (int x = 1; x <= i; x++)
+cost[i + i/x] = min(cost[i] + 1, cost[i + i/x]);
+
+; cin >> tc;
+(tc--) {
+cin >> n >> k;
+
+for (int i = 0; i < n; i++) cin >> b[i];
+for (int i = 0; i < n; i++) cin >> c[i];
+
+int s = accumulate(c, c+n, 0);
+
+if (n*15 <= k) {
+cout << s << endl;
+continue;
+}
+
+for (int i = 0; i < n+7; i++)
+for (int j = 0; j < k+7; j++)
+dp[i][j] = -1;
+
+cout << solve(0, k) << endl;
+}
+}
