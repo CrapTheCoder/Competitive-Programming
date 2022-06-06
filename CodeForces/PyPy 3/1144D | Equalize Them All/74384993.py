6a1cd55cfd1a8f8a614a1bc49d79b1174124b73e
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 03:12:32 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1144D | Equalize Them All/74384993.py
@@ -0,0 +1,30 @@
+from collections import Counter
+ 
+n = int(input())
+a = list(map(int, input().split()))
+ 
+c = Counter(a)
+ 
+m = max(c, key=c.get)
+ 
+x = a.index(m)
+ 
+print(n - c[m])
+ 
+for i in range(x-1, -1, -1):
+    if a[i] == m:
+        continue
+ 
+    if a[i] < m:
+        print(1, i+1, i+2)
+    else:
+        print(2, i+1, i+2)
+ 
+for i in range(x+1, n):
+    if a[i] == m:
+        continue
+ 
+    if a[i] < m:
+        print(1, i+1, i)
+    else:
+        print(2, i+1, i)
