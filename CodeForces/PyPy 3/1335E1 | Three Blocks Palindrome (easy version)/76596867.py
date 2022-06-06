706330fd5750d07b2f65455f8691acaf9f36e8ef
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 02:38:27 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1335E1 | Three Blocks Palindrome (easy version)/76596867.py
@@ -0,0 +1,33 @@
+from collections import Counter
+
+for _ in range(int(input())):
+n = int(input())
+a = list(map(int, input().split()))
+
+s = Counter(a)
+
+x = [(i, j) for i in s for j in s if i != j]
+
+maxi = 0
+
+for i, j in x:
+size = 0
+
+l = 0
+r = n - 1
+
+while True:
+while a[l] != i: l += 1
+while a[r] != i: r -= 1
+
+if l >= r:
+break
+
+size += 1
+
+maxi = max(maxi, size * 2 + a[l:r].count(j))
+
+l += 1
+r -= 1
+
+print(max(maxi, max(s.values())))
