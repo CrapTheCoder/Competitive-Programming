7d34dac38e69bfb10778ffd13eb8fe31a77854b5
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 01:18:47 +0530

    Committing files

---
+++ CodeForces/PyPy 3-64/1509B | TMT Document/155725712.py
@@ -0,0 +1,18 @@
+range(int(input())):
+int(input())
+input()
+
+count('M') * 2 != s.count('T'):
+print('NO')
+continue
+
+i for i, j in enumerate(s) if j == 'M']
+i for i, j in enumerate(s) if j == 'T']
+
+in range(n // 3):
+not t[c] < m[c] < t[c + n//3]:
+print('NO')
+break
+
+
+print('YES')
