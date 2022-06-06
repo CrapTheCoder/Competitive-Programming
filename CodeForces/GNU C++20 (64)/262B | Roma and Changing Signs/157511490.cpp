268f23d6c325005daf673e653ceeb85c2f3016b7
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 01:11:53 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/262B | Roma and Changing Signs/157511490.cpp
@@ -0,0 +1,30 @@
+bits/stdc++.h>
+namespace std;
+
+int long long
+
+main() {
+k; cin >> n >> k;
+
+multiset<int> s;
+int i = 0; i < n; i++) {
+int x; cin >> x;
+insert(x);
+
+
+(true) {
+auto x = *s.begin();
+(x > 0 || k == 0) break;
+
+erase(s.begin());
+insert(-x);
+--;
+
+
+% 2) {
+auto x = *s.begin();
+erase(s.begin());
+insert(-x);
+
+
+<< accumulate(s.begin(), s.end(), 0) << endl;
