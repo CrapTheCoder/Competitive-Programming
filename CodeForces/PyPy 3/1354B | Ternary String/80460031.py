c979e817269e9065fa7eb3cb2a37076f6bde5f57
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 02:37:12 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1354B | Ternary String/80460031.py
@@ -0,0 +1,28 @@
+from sys import stdin
+input = stdin.readline
+
+def min(x, y): return x if x < y else y
+def max(x, y): return x if x > y else y
+
+for _ in range(int(input())):
+s = list(map(int, input().strip()))
+
+d = {}
+
+mini = float('inf')
+
+for j, i in enumerate(s):
+d[i] = j
+
+if (1 in d) and (2 in d) and (3 in d):
+if i == 1:
+mini = min(mini, j - min(d[2], d[3]) + 1)
+if i == 2:
+mini = min(mini, j - min(d[1], d[3]) + 1)
+if i == 3:
+mini = min(mini, j - min(d[1], d[2]) + 1)
+
+if mini == float('inf'):
+print(0)
+else:
+print(mini)
