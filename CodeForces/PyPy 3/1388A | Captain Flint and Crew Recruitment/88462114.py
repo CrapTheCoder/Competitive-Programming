ac5320ea7c3be7ab59abce3925c448f1bcc8722e
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 03:08:45 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1388A | Captain Flint and Crew Recruitment/88462114.py
@@ -0,0 +1,18 @@
+d = [6, 10, 14]
+s = sum(d)
+ 
+for _ in range(int(input())):
+    n = int(input())
+ 
+    if n <= s:
+        print('NO')
+        continue
+ 
+    if n - s in d:
+        print('YES')
+        print(6, 10, 15, n-(6+10+15))
+        continue
+ 
+    else:
+        print('YES')
+        print(6, 10, 14, n-s)
