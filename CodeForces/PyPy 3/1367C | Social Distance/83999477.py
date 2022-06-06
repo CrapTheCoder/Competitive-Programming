98c7877cebae6790ccb4ca9109fde3f3f0cbfb8f
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 02:35:46 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1367C | Social Distance/83999477.py
@@ -0,0 +1,25 @@
+for _ in range(int(input())):
+n, k = map(int, input().split())
+s = input()
+n = len(s)
+
+if '1' not in s:
+print((n-1) // (k+1) + 1)
+continue
+
+l = -1
+c = 0
+
+for i, j in enumerate(s):
+if j == '1':
+if l == -1:
+c += i // (k+1)
+else:
+d = i-l-1
+c += max(0, (d - k * 2 - 1) // (k+1) + 1)
+
+l = i
+
+c += (n-l-1) // (k+1)
+
+print(c)
