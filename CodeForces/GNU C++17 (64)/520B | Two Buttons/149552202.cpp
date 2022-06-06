b593189a2f5869fdfbe2ec1723df2d519049580f
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 02:57:05 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/520B | Two Buttons/149552202.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+#define endl '\n'
+ 
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+ 
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+ 
+    int n, m;
+    cin >> n >> m;
+ 
+    int c = 0;
+    while (m > n) {
+        if (m % 2) c++, m++;
+        m /= 2; c++;
+    }
+ 
+    cout << c + (n - m) << endl;
+}
