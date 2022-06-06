dce7c8dc088da4c6d2f76851b6d19fba60ec1c1b
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 02:34:09 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1392C | Omkar and Waterslide/90375445.py
@@ -0,0 +1,11 @@
+for _ in range(int(input())):
+n = int(input())
+a = list(map(int, input().split()))
+
+t = 0
+mini = cur = a[0]
+
+for i in range(n-1, 0, -1):
+t += max(a[i-1] - a[i], 0)
+
+print(t)
