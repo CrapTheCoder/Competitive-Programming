f2c17e916dfa421814cd88fc4cf269e394dc2462
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 03:06:51 +0530

    Committing files

---
+++ CodeForces/PyPy 3/761B | Dasha and friends/95961439.py
@@ -0,0 +1,15 @@
+n, l = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+ 
+for i in range(l):
+    for j in range(n):
+        a[j] = (a[j] + 1) % l
+ 
+    a.sort()
+ 
+    if a == b:
+        print('YES')
+        break
+else:
+    print('NO')
