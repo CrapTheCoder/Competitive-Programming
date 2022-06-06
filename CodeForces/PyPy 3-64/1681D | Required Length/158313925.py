07898e5ca6fe3fbd3a2cfb63239ae122b45bb99c
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 01:11:23 +0530

    Committing files

---
+++ CodeForces/PyPy 3-64/1681D | Required Length/158313925.py
@@ -0,0 +1,25 @@
+c):
+(str(c)) > n:
+return float('inf')
+
+in dp:
+return dp[c]
+
+(str(c)) == n:
+return 0
+
+float('inf')
+in str(c):
+c * int(y) != c:
+d = solve(c * int(y)) + 1
+if d < mini:
+mini = d
+
+= mini
+mini
+
+(int, input().split())
+
+
+x)
+r != float('inf') else -1)
