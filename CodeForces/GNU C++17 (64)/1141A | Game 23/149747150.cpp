5296c8ffea2c3e079c39302aed2f6d222b3a1fc8
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 01:20:31 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1141A | Game 23/149747150.cpp
@@ -0,0 +1,41 @@
+bits/stdc++.h>
+namespace std;
+
+int long long
+endl '\n'
+
+MOD = 1e9 + 7;
+INF = LLONG_MAX >> 1;
+
+main() {
+sync_with_stdio(false); cin.tie(NULL);
+
+m;
+n >> m;
+
+% n != 0) {
+cout << -1 << endl;
+return 0;
+
+
+/ n;
+
+c2 = 0, c3 = 0;
+
+(n % 2 == 0) {
+/= 2;
+++;
+
+
+(n % 3 == 0) {
+/= 3;
+++;
+
+
+!= 1) {
+cout << -1 << endl;
+return 0;
+
+
+cout << c2 + c3 << endl;
+}
