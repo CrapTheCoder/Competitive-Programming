5ac90b28566ec38912c04aab9b32cdee7806c899
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 01:18:51 +0530

    Committing files

---
+++ CodeForces/PyPy 3-64/1512D | Corrupted Array/155724609.py
@@ -0,0 +1,17 @@
+range(int(input())):
+int(input())
+sorted(map(int, input().split()))
+
+(a[:-2]) in a[-2:]:
+print(*a[:-2])
+
+sum(a[:-1]) - a[-1] in a:
+remove(sum(a[:-1]) - a[-1])
+
+sum(a[:-1]) == a[-1]:
+print(*a[:-1])
+else:
+print(-1)
+
+
+print(-1)
