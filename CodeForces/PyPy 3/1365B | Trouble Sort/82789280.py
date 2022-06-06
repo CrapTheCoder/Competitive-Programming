e3819ce41f279f02da554c7e8ad1c177f2187b6b
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 03:09:27 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1365B | Trouble Sort/82789280.py
@@ -0,0 +1,16 @@
+from sys import stdin
+input = stdin.readline
+ 
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+ 
+    if len(set(b)) == 2:
+        print('Yes')
+        continue
+ 
+    if a == sorted(a):
+        print('Yes')
+    else:
+        print('No')
