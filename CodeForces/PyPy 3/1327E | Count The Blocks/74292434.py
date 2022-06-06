d9a14e79f34918aa4527a6f19a775749fbe75651
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 02:39:31 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1327E | Count The Blocks/74292434.py
@@ -0,0 +1,24 @@
+MOD = 998244353
+
+n = int(input())
+
+if n == 1:
+print(10)
+exit()
+
+a = [1] * (n-1)
+a[0] = 20
+
+for i in range(1, n-1):
+a[i] *= (a[i-1] * 10) + (9 * (pow(10, i, MOD)))
+a[i] %= MOD
+
+a.reverse()
+
+for i in range(n-1):
+a[i] *= 9
+a[i] %= MOD
+
+a.append(10)
+
+print(*a)
