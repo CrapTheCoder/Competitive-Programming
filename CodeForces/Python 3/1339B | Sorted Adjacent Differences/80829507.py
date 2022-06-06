298d16a69ace2c41cda93f32604c0f745dd7841e
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 03:10:21 +0530

    Committing files

---
+++ CodeForces/Python 3/1339B | Sorted Adjacent Differences/80829507.py
@@ -0,0 +1,16 @@
+for _ in range(int(input())):
+    n = int(input())
+    a = sorted(map(int, input().split()))
+ 
+    b = []
+ 
+    for i in range(n // 2):
+        b.append(a[i])
+        b.append(a[n-i-1])
+ 
+    if n % 2:
+        b.append(a[n // 2])
+ 
+    b.reverse()
+ 
+    print(*b)
