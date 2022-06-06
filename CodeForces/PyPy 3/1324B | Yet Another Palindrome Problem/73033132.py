15b246f6ce03bc95f73184ddb8f1deade24397c0
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 02:40:50 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1324B | Yet Another Palindrome Problem/73033132.py
@@ -0,0 +1,28 @@
+from collections import Counter
+
+for _ in range(int(input())):
+n = int(input())
+a = list(map(int, input().split()))
+
+C = Counter(a)
+
+if any(i > 2 for i in C.values()):
+print('YES')
+continue
+
+d = {}
+
+for i in range(n):
+d[a[i]] = d.get(a[i], -1)
+
+if d[a[i]] == -1:
+d[a[i]] = i
+continue
+
+j = d[a[i]]
+
+if i - j - 1 > 0:
+print('YES')
+break
+else:
+print('NO')
