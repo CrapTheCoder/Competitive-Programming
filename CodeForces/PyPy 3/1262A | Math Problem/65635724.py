90319a66152d204e856470e4531d45ad53d9e1a9
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 02:45:54 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1262A | Math Problem/65635724.py
@@ -0,0 +1,13 @@
+for _ in  range(int(input())):
+n = int(input())
+x = []
+
+m1 = -1
+m2 = float('inf')
+
+for __ in range(n):
+l, r = map(int, input().split())
+m1 = max(m1, l)
+m2 = min(m2, r)
+
+print(max(0, m1 - m2))
