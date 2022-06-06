7642fb62df4632bb8fcfd6c3d0e1c4aa34c9842d
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 03:14:31 +0530

    Committing files

---
+++ CodeForces/PyPy 3/1216B | Shooting/72286230.py
@@ -0,0 +1,12 @@
+n = int(input())
+a = list(map(int, input().split()))
+ 
+l = sorted(zip(a, range(1, n + 1)), reverse=True)
+ 
+c = 0
+ 
+for i in range(n):
+    c += l[i][0] * i + 1
+ 
+print(c)
+print(*(i[1] for i in l))
