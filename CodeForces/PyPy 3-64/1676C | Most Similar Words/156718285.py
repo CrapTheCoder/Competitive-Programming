0018d717a5715133fe6f14020a5e841bf9784dc7
crapthecoder <41336229+crapthecoder@users.noreply.github.com>
2022-06-07 01:12:39 +0530

    Committing files

---
+++ CodeForces/PyPy 3-64/1676C | Most Similar Words/156718285.py
@@ -0,0 +1,17 @@
+import stdin
+stdin.readline
+
+range(int(input())):
+map(int, input().split())
+input().strip() for _ in range(n)]
+
+float('inf')
+in range(n):
+for j in range(i+1, n):
+s = 0
+for k in range(m):
+s += abs(ord(l[i][k]) - ord(l[j][k]))
+
+mini = min(mini, s)
+
+mini)
